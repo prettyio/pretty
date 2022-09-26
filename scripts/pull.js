77003bcba18fbe0c1af79bc93ff6b520eb92d7fb
impossible98 <ago2chris@outlook.com>
2022-09-26 10:43:04 +0800

    chore: add script/pull.js

---
+++ scripts/pull.js
@@ -0,0 +1,25 @@
+const childProcess = require('node:child_process');
+
+function green(text) {
+  return `\x1b[32m${text}\x1b[0m`;
+}
+
+function main() {
+  childProcess.execFile('git', ['pull'], (error, _stdout, _stderr) => {
+    if (error) {
+      throw error;
+    }
+    const TIME = new Date();
+    const YEAR = TIME.getFullYear();
+    const MONTH =
+      TIME.getMonth() < 10 ? '0' + (TIME.getMonth() + 1) : TIME.getMonth() + 1;
+    const DAY = TIME.getDate() < 10 ? '0' + TIME.getDate() : TIME.getDate();
+    const HOUR = TIME.getHours();
+    const MINUTE = TIME.getMinutes();
+    const SECOND = TIME.getSeconds();
+    const formatTime = `${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}:${SECOND}`;
+    console.log(` ${formatTime} ${green('[INFO]')} Finished: git pull`);
+  });
+}
+
+main();
